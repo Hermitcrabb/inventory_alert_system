adfae328a6e3189cf275612293781e016221b7bd
Pratham <prathambhandari2000@gmail.com>
2026-01-22 09:03:35 +0545

    Csrf tokens exceptions

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,9 +4,7 @@ return [
 
 
     App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
     App\Providers\EventServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
     App\Providers\ShopifyServiceProvider::class,
 
 
